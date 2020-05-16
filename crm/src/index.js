001a94ed7f4a87e45a75923c2b8692ce2feefb31
jshinde <jshindems@gmail.com>
2020-05-15 22:56:13 -0400

    added components and data

--- crm/src/index.js
+++ crm/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router> 
+      <App />
+  </Router>,
+  
   document.getElementById('root')
 );
 
