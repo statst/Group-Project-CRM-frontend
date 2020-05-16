38c5e81605485380ab28852e020c91d6a0ed10f4
foolan <foolanbhosale5@gmail.com>
2020-05-16 15:11:51 -0400

    fetched data using backend api

--- crm/src/App.js
+++ crm/src/App.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react';
+import React from 'react';
+
 import './App.css';
 import NavBar from './components/Navbar/Navbar';
 import Clients from './components/Client/Client';
@@ -6,41 +7,41 @@ import clientsSeed from './Data/clientsSeed.json';
 import Users from './components/User/User';
 import usersSeed from './Data/usersSeed.json';
 
-
 import {BrowserRouter, Route} from 'react-router-dom';
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            clients: clientsSeed,
-            users: usersSeed
-        };
-    }
+const App = () => (
+    <div>
+        <NavBar />
+        <main>
+            <Route exact path='/users' component={Users} />
+        </main>
+    </div>
+);
 
-    render() {
-        console.log(this.state.clients);
-        console.log(this.state.users)
-        return (
-					<div>
-						<NavBar />
-						<main>
-							<Route
-								exact
-								path='/clients'
-								render={() => <Clients clients={this.state.clients} />}
-							/>
-							<Route
-								exact
-								path='/users'
-								render={() => <Users users={this.state.users} />}
-							/>
-							{/* <Clients clients={this.state.clients} /> */}
-							{/* <Users users={this.state.users} /> */}
-						</main>
-					</div>
-				);
-    }
-}
+// class App extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             clients: clientsSeed,
+//             users: usersSeed
+//         };
+//     }
+
+//     render() {
+//         console.log(this.state.clients);
+//         console.log(this.state.users);
+//         return (
+//             <div>
+//                 <NavBar />
+//                 <main>
+//                     <Route exact path='/clients' render={() => <Clients clients={this.state.clients} />} />
+//                     <Route exact path='/users' render={() => <Users users={this.state.users} />} />
+//                     {/* <Clients clients={this.state.clients} /> */}
+//                     {/* <Users users={this.state.users} /> */}
+//                 </main>
+//             </div>
+//         );
+//     }
+// }
 
 export default App;
--- crm/src/components/User/User.js
+++ crm/src/components/User/User.js
@@ -1,34 +1,76 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {APIURL} from '../../config';
 import users from '../../Data/usersSeed.json';
 import Table from 'react-bootstrap/Table';
 
 function Users(props) {
-	console.log(props.users);
-	return (
-		<div>
-			<h1>Users</h1>
-			{props.users &&
-				props.users.map((user) => {
-					return (
-						<div key={user._id} className='container'>
-							<p>{user.firstname}</p>
-							<p>{user.lastname}</p>
-							<p>{user.email}</p>
-							{/* <Table responsive ="sm">
-								<thead>
-									<tr>
-										<th>#</th>
-										<th>{user.firstname}</th>
-										<th>{user.lastname}</th>
-										<th>{user.email}</th>
-									</tr>
-								</thead>
-							</Table> */}
-						</div>
-					);
-				})}
-		</div>
-	);
+    const [users, setUsers] = useState([]);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        // load the movies when the component is mounted
+        fetch(`${APIURL}/api/users`)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                // Update state with successful movie data
+                setUsers(data);
+            })
+            .catch(() => {
+                // Update the state if there was an error
+                // so we can give feedback to the user!
+                setError(true);
+            });
+    }, []);
+    if (error) {
+        return <div>Sorry, there was a problem getting the users</div>;
+    }
+
+    // Check if we have our movies
+    // Display "Loading..." if not
+    if (users.length === 0) {
+        return <div>Loading...</div>;
+    }
+    return (
+        <div>
+            {users.map((user) => (
+                <div key={user._id}>
+                    <p>{user.firstname}</p>
+                    <p>{user.lastname}</p>
+                    <p>{user.email}</p>
+                </div>
+            ))}
+        </div>
+    );
 }
+
+// function Users(props) {
+//     console.log(props.users);
+//     return (
+//         <div>
+//             <h1>Users</h1>
+//             {props.users &&
+//                 props.users.map((user) => {
+//                     return (
+//                         <div key={user._id} className='container'>
+//                             <p>{user.firstname}</p>
+//                             <p>{user.lastname}</p>
+//                             <p>{user.email}</p>
+//                             {/* <Table responsive ="sm">
+// 								<thead>
+// 									<tr>
+// 										<th>#</th>
+// 										<th>{user.firstname}</th>
+// 										<th>{user.lastname}</th>
+// 										<th>{user.email}</th>
+// 									</tr>
+// 								</thead>
+// 							</Table> */}
+//                         </div>
+//                     );
+//                 })}
+//         </div>
+//     );
+// }
 
 export default Users;
