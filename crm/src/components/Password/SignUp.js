29bee316be0add1b31cfcc716b30964de952afff
ttdesign <tkthedesigner@gmail.com>
2020-05-20 11:55:55 -0700

    fixed signup and signin form

--- crm/src/components/Password/SignUp.js
+++ crm/src/components/Password/SignUp.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { APIURL } from '../../config';
 import './Password.css';
 
 class SignUp extends Component {
@@ -13,23 +15,43 @@ class SignUp extends Component {
 			passwordConfirm: '',
 			valid: true,
 			submit: false,
+			redirectToReferrer: false,
 		};
 	}
 	handleChange = (event) => {
 		this.setState({ [event.target.name]: event.target.value });
 	};
-	checkPassword = (event) => {
+
+	handleSubmit = (event) => {
 		event.preventDefault();
-		this.state.password === this.state.passwordConfirm
-			? this.setState({ valid: true, submit: true })
-			: this.setState({ valid: false, submit: true });
+		const url = `${APIURL}/api/users`;
+		fetch(url, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				firstname: this.state.firstname,
+				lastname: this.state.lastname,
+				email: this.state.email,
+				password: this.state.password,
+			}),
+		})
+			.then((response) => response.json())
+			.then((response) => {
+				this.setState({ ...this.state, redirectToReferrer: true });
+			});
 	};
 
 	render() {
+		const { redirectToReferrer } = this.state;
+		if (redirectToReferrer) {
+			return <Redirect to='/signin' />;
+		}
 		return (
 			<div className='form'>
 				<h1>Sign Up</h1>
-				<form onSubmit={this.checkPassword}>
+				<form onSubmit={this.handleSubmit}>
 					<input
 						type='text'
 						placeholder='First Name'
@@ -57,15 +79,6 @@ class SignUp extends Component {
 						onChange={this.handleChange}
 					/>
 					<label htmlFor='lastname'>Email</label>
-					<input
-						type='text'
-						placeholder='Username'
-						id='username'
-						name='username'
-						value={this.state.username}
-						onChange={this.handleChange}
-					/>
-					<label htmlFor='username'>Username</label>
 
 					<input
 						type='password'
@@ -76,24 +89,7 @@ class SignUp extends Component {
 						onChange={this.handleChange}
 					/>
 					<label htmlFor='password'>Password</label>
-
-					<input
-						type='password'
-						placeholder='Confirm password'
-						id='passwordConfirm'
-						name='passwordConfirm'
-						value={this.state.passwordConfirm}
-						onChange={this.handleChange}
-					/>
-					<label htmlFor='passwordConfirm'>Confirm password</label>
-
 					<button type='submit'>Sign Up</button>
-					{this.state.submit && (
-						<p className={this.state.value ? 'valid' : 'invalid'}>
-							{this.state.valid ? 'password matched' : 'password do not match'}
-						</p>
-					)}
-					{!this.state.submit}
 				</form>
 			</div>
 		);
