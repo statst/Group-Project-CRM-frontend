497fe75f445de97bcb544d08613a6d4742fb29a8
foolan <foolanbhosale5@gmail.com>
2020-05-21 00:18:29 -0400

    Navbar component

--- crm/src/components/Navbar/Navbar.js
+++ crm/src/components/Navbar/Navbar.js
@@ -1,24 +1,39 @@
 import React from 'react';
-// import Navbar from 'react-bootstrap/Navbar';
-// import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 function NavBar({ userToken, handleSignOut }) {
 	return (
 		<>
-			<Link to='/'>Home</Link>
-			<Link to='/features'>Features</Link>
-			<Link to='/contact'>Contact Us</Link>
-			{userToken ? <Link to='/api/clients'>Clients</Link> : ''}
-			{userToken ? <Link to='/api/users'>Users</Link> : ''}
-			{/* <Link to='/api/users/:edit'>Users</Link> */}
-			{!userToken ? <Link to='/signin'>Sign In</Link> : ''}
 
-			{userToken ? <Link to='/dashboard'>Dashboard</Link> : ''}
-			{userToken ? 
-				<Link to='/' onClick={handleSignOut} className='btn btn-danger ml-0'>
+<Navbar expand='lg'>
+            <Navbar.Brand href='/'>CRM</Navbar.Brand>
+            <Navbar.Toggle aria-controls='basic-navbar-nav' />
+            <Navbar.Collapse id='basic-navbar-nav'>
+                <Nav className='ml-auto'>
+                    <Link to='/'>Home</Link>
+                    <Link to='/features'>Features</Link>
+					<Link to='/contact'>Contact Us</Link>
+					  {userToken ? <Link to='/api/clients' >Clients</Link> : ''}
+              {userToken ? <Link to='/api/users'>Users</Link> : ''}
+            {!userToken ? <Link to='/signin'>Sign In</Link> : ''}
+			 {userToken ? <Link to='/dashboard'>Dashboard</Link> : ''}
+			 {userToken ? 
+				<Link to='/' onClick={handleSignOut} className='btn btn-danger'>
 					Sign Out
 				</Link> : ''}
+                    
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+
+
+       
+	
+		
+			
 		</>
 	);
 }
