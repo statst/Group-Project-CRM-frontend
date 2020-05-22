e63120c8737d28557b3cc2e82e2e52a3e173fbd5
foolan <foolanbhosale5@gmail.com>
2020-05-21 21:53:21 -0400

    updated css for user component

--- src/components/User/UserDetails.js
+++ src/components/User/UserDetails.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { APIURL } from '../../config';
 import { Link, Redirect } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
 import UserForm from './UserForm';
 import UserCommunications from '../Communications/UserCommunications';
 import UserTransactions from '../Transactions/UserTransaction';
+import Divider from '@material-ui/core/Divider';
+import './User.css';
 
 const UserDetails = (props) => {
      const [user, setUser] = useState(null);
@@ -58,36 +61,50 @@ const UserDetails = (props) => {
 				{!user ? (
 					''
 				) : (
-					<div>
-						<p>First Name :{user.firstname} </p>
-						<p>lastName : {user.lastname}</p>
-						<p>Email:{user.email}</p>
-						<div>
+					<div className='col-md ml-3 mt-3 main'>
+				
+					<Card style={{ width: '20rem' }} >
+						<Card.Body>
+							<i className="far fa-address-card fa-3x"></i>
+						
+							<Card.Text><strong>First Name:</strong> {user.firstname}</Card.Text>
+							<Card.Text>Last Name: {user.lastname}</Card.Text>
+							<Card.Text>Email:{user.email}</Card.Text>
+							
+						</Card.Body>
+					</Card>
+				</div>
+					)}
+						<div className='mt-5 link'>
 							<Link
-								className='btn btn-info btn-sm margin-0'
+								className='btn btn-info item'
 								to={`/api/users/${emailId}/edit`}>
 								Edit
 							</Link>
 
 							<button
 								onClick={onDeleteUser}
-								className='btn btn-danger mr-3 ml-3'>
+								className='btn btn-danger item'>
 								Delete
 							</button>
-							<Link className='btn btn-info btn-md margin-0' to={`/api/users`}>
+							<Link className='btn btn-info item' to={`/api/users`}>
 								Go Back
 							</Link>
 						</div>
-					</div>
-				)}
-				<div>
-					<strong>Communications</strong>
-					<UserCommunications emailId={emailId} userToken={props.userToken} />
-				</div>
-				<div>
-					<strong>Transactions</strong>
+					
+
+					<div className='trans'>
+					<h3 className='sub-heading'>	<strong>Transactions <i className="far fa-credit-card"></i></strong></h3>
+					<Divider />
 					<UserTransactions emailId={emailId} userToken={props.userToken} />
 				</div>
+			
+				<div className='communication'>
+					<h3 className='sub-heading'><strong>Communications<i className="fas fa-tty"></i></strong></h3>
+					<Divider />
+					<UserCommunications emailId={emailId} userToken={props.userToken} />
+				</div>
+				
 			</>
 		);
 };
