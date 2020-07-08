a2b740858505ec7a3dab95b60807bc234aae637d
jyoti <jshindems@gmail.com>
2020-07-08 10:36:48 -0400

    removed unnecessary changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,28 +4,26 @@ import './Footer.css';
 
 const Footer = () => {
     return (
-        <div className= 'footer'>
-        <MDBFooter color = 'blue' className=' '>
-            <MDBContainer fluid className='text-center text-md-left'>
-                <MDBRow>
-                    <MDBCol md='6'>
-                        <p className='text-copy'>
-                            &copy; {new Date().getFullYear()} Copyright: Rory Ellis, TK Nguyen, Foolan Bhosale & Jyoti
-                            Shinde
-                        </p>
-                    </MDBCol>
-                    <MDBCol md='6'>
-                        <p className='title-list'>
-                            Our Team:
-                            <a href='https://www.linkedin.com/in/roryellis/'>Rory Ellis</a>
-                            <a href=' https://www.linkedin.com/in/tuongknguyen/'>TK Nguyen</a>
-                            <a href='https://www.linkedin.com/in/foolan-bhosale'>Foolan Bhosale</a>
-                            <a href='https://www.linkedin.com/in/jyoti-shinde/'>Jyoti Shinde</a>
-                        </p>
-                    </MDBCol>
-                </MDBRow>
-            </MDBContainer>
-        </MDBFooter>
+        <div className='footer'>
+            <MDBFooter color='blue' className=' '>
+                <MDBContainer fluid className='text-center text-md-left'>
+                    <MDBRow>
+                        <MDBCol md='6'>
+                            <p className='text-copy'>
+                                &copy; {new Date().getFullYear()} Copyright: Rory Ellis, TK Nguyen, & Jyoti Shinde
+                            </p>
+                        </MDBCol>
+                        <MDBCol md='6'>
+                            <p className='title-list'>
+                                Our Team:
+                                <a href='https://www.linkedin.com/in/roryellis/'>Rory Ellis</a>
+                                <a href=' https://www.linkedin.com/in/tuongknguyen/'>TK Nguyen</a>
+                                <a href='https://www.linkedin.com/in/jyoti-shinde/'>Jyoti Shinde</a>
+                            </p>
+                        </MDBCol>
+                    </MDBRow>
+                </MDBContainer>
+            </MDBFooter>
         </div>
     );
 };
